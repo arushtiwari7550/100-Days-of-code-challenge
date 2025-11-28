3f3530d275c8eba1519eeea4436a6f041841fff4
arushtiwari7550 <arushtiwari7550@gmail.com>
2025-11-29 00:19:17 +0530

    Add files via upload

--- day72_q1.c
+++ day72_q1.c
@@ -1,5 +1,5 @@
-/* Q122 Write a C program that opens an existing file (e.g., info.txt) and reads its contents using 
-fgets(). The program should print all the lines to the console until EOF (end of file) is reached.*/
+/* Q126 Ask the user for a filename. Check if it exists by trying to open it in read mode. If 
+the file pointer is NULL, print an error message; otherwise, read and display its content. */
 
 #include <stdio.h>
 
@@ -7,15 +7,21 @@ int main()
 {
     FILE *fp;              // File pointer
     char buffer[100];      // Buffer to store each line
+    char file[100];  // To store the entred file name
+
+    printf(" \nEnter a File name: ");
+    scanf("%99s", file);
 
     // Open the file in read mode
-    fp = fopen("content-plan.txt", "r");
+    fp = fopen( file , "r");
     if (fp == NULL) 
     {
-        printf("Error: Could not open file content-plan.txt\n");
+        printf("Error: File does not exist!\n");
         return 1; // Exit if file couldn't be opened
     }
 
+    printf("\nFile opened successfully.\n");
+    printf("\n========= File Content =========\n \n");
     // Read and print each line until EOF
     while (fgets(buffer, sizeof(buffer), fp) != NULL) 
     {
@@ -24,6 +30,8 @@ int main()
 
     // Close the file
     fclose(fp);
+
+    printf("\n ");
 
     return 0;
 }
